a1debb416865ca946949048b974bdee721cee576
imunderthetree <119784675+imunderthetree@users.noreply.github.com>
2025-03-22 17:51:21 +0200

    29. Divide Two Integers.py

--- 29. Divide Two Integers.py
+++ 29. Divide Two Integers.py
@@ -1,4 +1,12 @@
-x=int(input("Enter The Dividend ="))
-y=int(input("Enter The Divisor = "))
-D= x//y
-print("The Answer is = ", D)
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        ans=dividend//divisor
+        if ans==0:
+            return 0
+        if dividend%divisor==0:
+            return ans
+        if ans>0:
+            return math.floor(ans)
+        return ans+1
